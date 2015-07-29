7e82f01d2379a38c0cc72e80f0445be8902a7444
KshitizSethia <kshitizsethia@gmail.com>
2015-07-29 08:45:58 -0400

    DataCreators.LDAModel 	doc2bow in parallel 	using LDAmulticore, commented.. checkin to test on linux

--- server/DataCreators/LDAModel.py
+++ server/DataCreators/LDAModel.py
@@ -6,6 +6,7 @@ from threading import Thread
 from gensim import corpora
 from gensim.corpora.dictionary import Dictionary
 from gensim.models.ldamodel import LdaModel
+from gensim.models.ldamulticore import LdaMulticore
 
 from DataCreators import ArticleDB
 from Logger import logger
@@ -14,13 +15,13 @@ import TextTools
 import cPickle as pickle
 from string_constants import file_lda_model, file_gensim_dictionary, file_word_corpus,\
     file_articleIDToLDA
-from gensim.models.ldamulticore import LdaMulticore
+import functools
 
 
 # global settings for parallelization
-numProcesses = 4
-#keep chunksize low so that the result  array is not appended to at once.
-chunkSize = 100
+numProcesses = 3
+# keep chunksize low so that the result  array is not appended to at once.
+chunkSize = 50
 
 
 def load():
@@ -29,27 +30,6 @@ def load():
     gensim_dictionary = Dictionary.load(file_gensim_dictionary)
     article_id_to_LDA_dictionary = pickle.load(open(file_articleIDToLDA, "rb"))
     return lda_model, gensim_dictionary, article_id_to_LDA_dictionary
-
-
-def multiGetCleanedWords(articles):
-    result = []
-    for article in articles:
-        result.append((article[0], getCleanedWords(article[1])))
-    return result
-
-
-def parallelGetCleanedWords(article):
-    return article[0], getCleanedWords(article[1])
-
-
-def parallelGetWordCorpus(articleDB):
-    articles = articleDB.items()
-    pool = Pool()#processes=numProcesses)
-    results = pool.map(parallelGetCleanedWords, articles, chunksize=chunkSize)
-
-    logger.info("Back from multiprocessing, making dict now")
-    word_corpus = dict(results)
-    return word_corpus
 
 
 def getWordCorpus(articleDB):
@@ -64,6 +44,41 @@ def getWordCorpus(articleDB):
         "Saving word_corpus temporarily, in case the script ahead fails")
     pickle.dump(word_corpus, open(file_word_corpus, "wb"), protocol=2)
     return word_corpus
+
+
+def getBoWCorpus(dictionary, word_corpus_values):
+    return [dictionary.doc2bow(words) for words in word_corpus_values]
+
+
+def parallelGetCleanedWords(article):
+    return article[0], getCleanedWords(article[1])
+
+
+def parallelGetWordCorpus(articleDB):
+    articles = articleDB.items()
+    pool = Pool()  # processes=numProcesses)
+    results = pool.map(parallelGetCleanedWords, articles, chunksize=chunkSize)
+
+    logger.info("Back from multiprocessing, making dict now")
+    word_corpus = dict(results)
+    return word_corpus
+
+
+def _doc2bow_alias(dictionary, words):
+    """
+    Alias for instance method that allows the method to be called in a 
+    multiprocessing pool
+    see link for details: http://stackoverflow.com/a/29873604/681311
+    """
+    return dictionary.doc2bow(words)
+
+
+def parallelGetBoWCorpus(dictionary, word_corpus_values):
+    bound_instance = functools.partial(_doc2bow_alias, dictionary)
+
+    pool = Pool(processes=numProcesses)
+    result = pool.map(bound_instance, word_corpus_values, chunksize=chunkSize)
+    return result
 
 
 def create_and_save_model():
@@ -93,12 +108,15 @@ def create_and_save_model():
     logger.info("saving dictionary")
     dictionary.save(file_gensim_dictionary)
 
+    #dictionary = Dictionary.load(file_gensim_dictionary)
+
     logger.info("Creating BoW representations from articles")
-    bow_corpus = [dictionary.doc2bow(words) for words in word_corpus.values()]
+    bow_corpus = parallelGetBoWCorpus(dictionary, word_corpus.values())
 
     logger.info("Training LDA model")
     num_topics = int(math.log(len(word_corpus.keys())) + 1)  # assumption:
-    lda_model = LdaMulticore(bow_corpus, num_topics=num_topics, workers=numProcesses)
+    lda_model = LdaModel(
+        bow_corpus, num_topics=num_topics)#, workers=numProcesses)
 
     logger.info("Saving LDA model")
     lda_model.save(file_lda_model)
